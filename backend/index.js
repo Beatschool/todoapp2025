bc9f61f18898e9e8f845394856e1a09d80d5bc15
Beata <dbharrisson94@outlook.com>
2025-08-25 18:39:13 +0800

    First commit of files for ToDoApp

---
+++ backend/index.js
@@ -0,0 +1,220 @@
+// --------------------------- ↓ SETTING UP DEPENDENCIES ↓ --------------------------------
+require("dotenv").config();
+const express = require("express");
+const cors  = require("cors");    // Enables Cross Origin Resource Sharing
+const mongoose = require("mongoose");    
+
+// ---------------------------- ↓ INITIAL APP CONFIGURATION ↓ -----------------------------
+const port = process.env.PORT || 3000; 
+const app = express();    
+
+// -------------------------------- ↓ MIDDLEWARE SETUP ↓ -----------------------------------
+
+app.use(express.json());
+app.use(cors("*"));
+
+
+
+// ---------------------------------- ↓ API ROUTES ↓ --------------------------------------
+
+// app.get("/example", async (req, res) =>{
+//     res.send("Hello I am a messages form the backend");
+// })
+// console.log("day");
+
+
+
+// -----------------------------Database Connection + app start up ----------------------------------------
+
+
+
+
+
+(async() => {
+    try {
+        mongoose.set("autoIndex", false);
+
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ MongoDB Connected!");
+
+        await Task.syncIndexes();
+        console.log("✅ Indexes created!")  
+
+        app.listen(port, () => {
+            console.log(`to do app is live on ${port}`);
+        });
+    
+    } catch (error) {
+        console.error("❌Startup error:", error);
+        process.exit(1);
+        
+    }
+
+})();
+
+// data structure
+const taskSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    dueDate: {type: Date, required: true},
+    dateCreated: {type: Date, default: Date.now, required: true},
+    completed: {type: Boolean, required: true, default: false} 
+});
+
+taskSchema.index({duedate: 1 });
+taskSchema.index({ dateCreated: 1});
+
+
+const Task = mongoose.model("Task", taskSchema);
+
+
+
+
+
+
+
+// -------------------------------------- TASK ROUTES --------------------------------------
+
+// let taskId = 1;
+
+// const tasks = [
+// {id: taskId++, completed: true, title: "Wash car",description:"My car is filthy and needs it's annual clean", dueDate:"10/08/25", createdOn: "28/07/2025" },
+// {id: taskId++, completed: true, title: "Make dinner",description:"we need to eat ", dueDate:"01/08/25", createdOn: "28/07/2025" },
+// {id: taskId++, completed: false, title: "Play games",description:"Keep Daughter entertained", dueDate:"25/10/25", createdOn: "28/07/2025" },
+// {id: taskId++, completed: false, title: "Read book",description:"Enjoy a new book", dueDate:"10/11/25", createdOn: "28/07/2025" },
+
+// ];
+
+
+
+app.get("/tasks", async(req, res) => {
+    try {
+        const{ sortBy } = req.query;
+
+        let sortOption = {};
+
+        if (sortBy === "dueDate"){
+            sortOption = { dueDate: 1}            
+        } else if (sortBy === "dateCreated") {
+            sortOption = { dateCreated: 1 };
+        }
+
+        const tasks = await Task.find({}).sort(sortOption);
+        res.json(tasks);
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Error grabbing tasks!"});
+    }
+});  
+
+
+
+app.post("/tasks/todo", async (req, res) => {
+    try {
+
+        const {title, description,dueDate} = req.body;
+
+        const taskData = { title, description, dueDate };
+        const createTask = new Task(taskData);
+        const newTask = await createTask.save();
+
+        res.json({ task: newTask, massage: "New task created successfully!"});
+
+    }   catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Error creating the tasks!"});
+    }
+})
+
+
+app.patch("/tasks/complete/:id", async (req, res) => {
+    try {
+        const { completed } = req.body;
+        const taskId = req.params.id;
+
+        const completedTask = await Task.findByIdAndUpdate(taskId, { completed }, {new: true});
+
+        if (!completedTask) {
+            return res.status(404).json({ message: "Task not found!" });
+        }
+        
+        res.json({ task: completedTask, message: "Task set to 'complete'"});
+        
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: " Error completing the task!"});
+        
+    }
+});
+
+
+app.patch("/tasks/notComplete/:id", async(req, res) => {
+    try {
+        const { completed } = req.body;
+        const taskId = req.params.id;
+
+        const taskNotComplete = await Task.findByIdAndUpdate(taskId, {completed}, {new: true});
+
+        if (!taskNotComplete) {
+            return res.status(404).json({ message: "Task not found" });
+
+        }
+        res.json({ task: taskNotComplete, message: "Task set to 'not complete'"});
+        
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: " Error setting the task to 'not complete'!"});
+        
+    }
+
+});
+
+
+app.delete("/tasks/delete/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const deletedTask = await Task.findByIdAndDelete(taskId);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found"});
+
+        }
+        res.json({ task: deletedTask, message: "Task deleted successfully" });
+        
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Error deleting the task!" });        
+    }
+});
+
+
+
+app.put("/tasks/update/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const { title, description, dueDate } = req.body;
+
+        const taskData = { title, description, dueDate };
+        const updatedTask = await Task.findByIdAndUpdate(taskId, taskData, { new: true});
+
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });            
+        }
+        
+        res.json({ task: updatedTask, message: "Task updated successfully!" });
+
+    } catch (error) {        
+        console.error("Error:", error);
+        res.status(500).json({ message: " Error editing the task!" });            
+    }
+});
+
+
+
+
+
+
+
+
+
---
+++ frontend/js/api.js
@@ -0,0 +1,354 @@
+// ------------------ ↓ GLOBAL VARIABLES (ALLOWED TO BE USED IN EVERY FUNCTION ONWARDS) ↓ ------------------
+
+// const { response } = require("express");
+// const { describe } = require("node:test");
+
+const taskForm = document.getElementById("taskForm");
+const toDoList = document.getElementById("toDoList");
+const completedList = document.getElementById("completedList");
+const URL = "http://localhost:3000";
+
+
+// ----------------------------------------- ↓ GENERAL FUNCTIONS ↓ -----------------------------------------
+
+function resetForm() {
+    taskForm.reset();
+}
+
+
+// ----------------------------- ↓ GENERAL EVENT LISTENERS (TRIGGERS) ↓ ---------------------------------
+
+const sortButton = document.getElementById("sortSelect");
+window.addEventListener("DOMContentLoaded", () => {
+    sortButton.value = "default";
+});
+
+sortButton.addEventListener("change", () => {
+    displayTasks();
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayTasks();
+});
+
+
+// ----------------------------- ↓ EVENT LISTENERS (TRIGGERS) FOR TASKS ↓ ---------------------------------
+
+taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    createNewTask();
+});
+
+toDoList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("done")) {
+
+        const taskId = event.target.getAttribute("data-id");
+        completeTask(taskId);
+    }
+});
+
+completedList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("notDone")) {
+
+        const taskId = event.target.getAttribute("data-id");
+        taskNotCompleted(taskId);  
+        
+    }
+});
+
+
+[toDoList, completedList].forEach(list => {
+    list.addEventListener("click", (event) => {
+        if (event.target.classList.contains("delete")) {
+
+        const taskId = event.target.getAttribute("data-id");
+        deleteTask(taskId);          
+        } 
+    })
+    
+});
+
+toDoList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("edit")) {
+       
+        const taskId = event.target.getAttribute("data-id");
+        const taskTitle = event.target.getAttribute("data-title");
+        const taskDescription = event.target.getAttribute("data-description");
+        const taskDueDate = new Date(event.target.getAttribute("data-due-date"));
+
+        const editTaskName = document.getElementById("editTaskName");
+        const editTaskDescription = document.getElementById("editTaskDescription");
+        const editDueDate = document.getElementById("editDueDate");
+        const saveChangesButton = document.getElementById("saveChangesButton");
+
+
+        editTaskName.value = taskTitle;
+        editTaskDescription.value = taskDescription;
+        
+        const formattedDueDate = taskDueDate.toISOString().split("T")[0];
+
+        editDueDate.value = formattedDueDate;
+        
+        saveChangesButton.addEventListener("click", async () => {
+            await editTask(taskId);
+
+            const editTaskModal = bootstrap.Modal.getInstance(document.getElementById("editTaskWindow"));
+            editTaskModal.hide();
+        }, { once: true });
+    
+    }
+});
+
+// Monthy.addEventListener("click", (event) => {
+
+// }
+//     )
+
+
+
+
+
+
+
+// --------------------------------------------- ↓ TASK FUNCTIONS ↓ ---------------------------------------------
+    
+
+
+async function displayTasks() {
+    try {
+        const sortSelect = document.getElementById("sortSelect");
+        const sortBy = sortSelect.value;
+        let query = "";
+        if (sortBy !== "default") {
+            query = `?sortBy=${sortBy}`;
+        }
+
+        const response = await fetch(`${URL}/tasks${query}`);
+
+        if (!response.ok) {
+            throw new Error(`Failed to get tasks: ${response.status}`)
+        }
+
+        const data = await response.json();
+
+        function formatTask(task) {
+        const li = document.createElement("li");
+        li.classList.add("p-3", "shadow-sm", "mt-2", "card");
+        li.innerHTML = task.completed ?
+        `
+        <div class="d-flex justify-content-between align-items-center">
+            <h4 class="col-11 text-decoration-line-through opacity-50">${task.title}</h4>
+            <button data-id="${task._id}" type="button" class="btn-close delete" aria-label="Close"></button>
+        </div>
+        <p class="text-decoration-line-through opacity-50">${task.description}</p>
+        <p class="text-decoration-line-through opacity-50"><strong>Due: </strong>${new Date(task.dueDate).toLocaleDateString()}</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <button data-id="${task._id}" class="btn btn-dark shadow-sm notDone" type="button"> Not done</button>                                        
+            </div>
+            <p class="m-0 text-decoration-line-through opacity-50"><strong>Created on: </strong>${new Date(task.dateCreated).toLocaleDateString()}</p>
+        </div>  
+        `
+        :
+        `
+        <div class="d-flex justify-content-between align-items-center">
+            <h4 class="col-11">${task.title}</h4>
+            <button data-id="${task._id}" type="button" class="btn-close delete" aria-label="Close"></button>
+        </div>
+        <p>${task.description}</p>
+        <p><strong>Due: </strong>${new Date(task.dueDate).toLocaleDateString()}</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <button data-bs-toggle="modal" data-bs-target="#editTaskWindow" data-id="${task._id}" data-title="${task.title}" data-description="${task.description}" data-due-date="${task.dueDate}" class="btn btn-dark shadow-sm edit" type="button"> edit</button>
+                <button  data-id="${task._id}" class="btn btn-dark shadow-sm done" type="button"> Done</button>
+            </div>
+            <p class="m-0"><strong>Created on: </strong>${new Date(task.dateCreated).toLocaleDateString()}</p>
+        </div>  
+        `;
+        return li;
+    }
+    // wipes column data to prevent double ups
+    toDoList.innerHTML = "";
+    completedList.innerHTML = "";
+
+    const tasks = data;
+    
+    tasks.forEach(task => {           
+        task.completed ? completedList.appendChild(formatTask(task)) : toDoList.appendChild(formatTask(task));
+        
+    });
+    // Resets the "Create New Tasks" form
+    taskForm.reset();
+       
+    } catch (error) {
+        console.error("Error:", error);
+        
+    }
+}
+    
+async function createNewTask() {
+
+    try {
+        const taskDetails = {
+            title: document.getElementById("taskName").value.trim(),
+            description: document.getElementById("taskDescription").value.trim(),
+            dueDate: document.getElementById("dueDate").value
+        };
+
+        const response = await fetch(`${URL}/tasks/todo`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(taskDetails)    
+        });
+
+        if (!response.ok) {
+        throw new Error(`Failed to create new task: ${response.status}`)
+        }
+
+        const data = await response.json();
+        console.log("New task created:", data);
+         displayTasks();
+         
+    } catch (error) {
+        console.error("Error", error);
+        
+    }
+   
+
+}
+
+async function completeTask(id) {
+   try {
+    const response = await fetch(`${URL}/tasks/complete/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ completed: true })
+            
+       });   
+        
+    if (!response.ok) {
+        throw new Error(`Failed to complete task: ${response.status}`);        
+    }
+    const data = await response.json();
+    console.log("Task completed", data);
+
+    displayTasks();
+    
+   } catch (error) {
+        console.error("Error:", error);
+   } 
+    
+}
+    
+async function taskNotCompleted(id) {
+       try {
+        const response = await fetch(`${URL}/tasks/notComplete/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ completed: false })            
+       });   
+        
+        if (!response.ok) {
+            throw new Error(`Failed to set task to 'not complete': ${response.status}`);        
+        }
+
+        const data = await response.json();
+        console.log("Task is set to 'not completed'", data);
+
+        displayTasks();    
+   } catch (error) {
+        console.error("Error:", error);
+   } 
+   
+}
+
+async function deleteTask(id) {
+   try {
+    const response = await fetch(`${URL}/tasks/delete/${id}`,{
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!response.ok)
+        throw new Error(`Failed to delete task! ${response.status}`);
+
+    const data = await response.json();
+    console.log({ message: "Deleted task", task: data });
+    displayTasks();
+    
+   } catch (error) {
+    console.error("Error:", error);
+
+    
+   }
+}
+
+async function editTask(id) {
+    try {
+        const updateDetails = {
+            title: document.getElementById("editTaskName").value.trim(),
+            description: document.getElementById("editTaskDescription").value.trim(),
+            dueDate: document.getElementById('editDueDate').value
+        };
+        const response = await fetch(`${URL}/tasks/update/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updateDetails)
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Failed to edit task: ${response.status}`);            
+        }
+        const data = await response.json();
+        console.log("Edited task:", data);
+        displayTasks();
+
+    } catch (error) {
+        console.error("Error:", error);
+        
+    }
+    
+}
+
+
+function Monthy(id) {
+
+    const updatePrice = {
+        stPrice: document.getElementById("standPrice"),
+        plPrice: document.getElementById("plusPrice"),
+        prPrice: document.getElementById("proPrice")
+    }
+
+    const Moye ={
+        document.getElementById(Moye)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// async function getExample() {
+//     const response = await fetch("http://localhost:3000/example");
+//     const data = await response.text();
+
+//     alert(data);
+// }
+
+// getExample();
